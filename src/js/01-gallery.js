c10605e86e90e1ee152050483a622c4fa3d54677
OlgaBilynska <olgabilinskaya@gmail.com>
2023-05-29 21:20:25 +0300

    including task 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,10 +1,6 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
 import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
 import "simplelightbox/dist/simple-lightbox.min.css";
-console.log(galleryItems);
 
 const makeGalleryMarkup = images => {
     return images.map(({ preview, original, description }) => {
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,27 @@
+import Vimeo from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo(iframe);
+    
+
+player.on('timeupdate', throttle(() => {
+    player.getCurrentTime().then(function (seconds) {
+        console.log(seconds);
+        localStorage.setItem("videoplayer-current-time", seconds);
+    }).catch(function (error) {
+        console.log(error);
+    });
+    }, 1000));
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time")).then(function (seconds) {
+    console.log(seconds); 
+}).catch(function(error) {
+    switch (error.name) {
+        case 'RangeError':
+            break;
+        default:
+            break;
+    }
+});
 
